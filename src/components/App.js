baf05dbfb6ddb4073921975110521e2c8209b33a
redebron20 <redebron20@gmail.com>
2021-08-08 15:25:20 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { respond } from 'fetch-mock'
 import React from 'react'
 
 import Filters from './Filters'
@@ -15,6 +16,34 @@ class App extends React.Component {
     }
   }
 
+  fetchPets = event => {
+    let endpoint = '/api/pets';
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(endpoint)
+      .then(respond => respond.json())
+      .then(pets => this.setState({pets: pets}))
+
+  }
+
+  onChangeType = event => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: event.target.value
+      }
+    })
+  }
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === petId ? {...pet, isAdopted: true} : pet
+    })
+    this.setState({ pets: pets });
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +53,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
